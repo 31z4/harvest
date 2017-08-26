a674d99d8525943ad8b1c9756be538a665849147
Elisey Zanko <elisey.zanko@gmail.com>
2017-08-26 18:31:21 +0500

    Test sampler

--- sampler_test.go
+++ sampler_test.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"os"
 	"strconv"
 	"testing"
+
+	"fmt"
+
+	"github.com/go-redis/redis"
 )
+
+var client = redis.NewClient(&redis.Options{Addr: "localhost:6379"})
 
 func TestSampleArguments(t *testing.T) {
 	cases := []struct {
@@ -40,4 +47,48 @@ func TestSampleArguments(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestSample(t *testing.T) {
+	output, err := Sample("redis://localhost:6379", 1, 1)
+	if err == nil {
+		t.Errorf("expected an error but got: %#v", output)
+	}
+
+	err = client.Set("key", "value", 0).Err()
+	if err != nil {
+		t.Errorf("SET: error: %#v\n", err)
+	}
+
+	output, err = Sample("redis://localhost:6379", 1, 1)
+	if err != nil {
+		t.Errorf("unexpected error: %#v", err)
+	}
+
+	if output != "key: 100.00% (1)" {
+		t.Errorf("unexpected output: %#v", output)
+	}
+}
+
+func TestMain(m *testing.M) {
+	dbSize, err := client.DBSize().Result()
+	if err != nil {
+		fmt.Printf("DBSIZE: error: %#v\n", err)
+		os.Exit(1)
+	}
+
+	if dbSize > 0 {
+		fmt.Println("redis database is not empty")
+		os.Exit(1)
+	}
+
+	returnCode := m.Run()
+
+	err = client.FlushDB().Err()
+	if err != nil {
+		fmt.Printf("FLUSHDB: error: %#v\n", err)
+		os.Exit(1)
+	}
+
+	os.Exit(returnCode)
 }
