03c15678d2da71017cfda8824ed9f27a89507a1f
Elisey Zanko <elisey.zanko@gmail.com>
2017-08-27 12:27:54 +0500

    Minor help cnahges

--- main.go
+++ main.go
@@ -13,9 +13,9 @@ func main() {
 	kingpin.Version("0.1.0")
 	kingpin.CommandLine.Help = "harvest helps you understand what's inside your Redis."
 
-	samples := kingpin.Flag("samples", "maximum number of samples").Short('s').Default("1000").Int()
-	results := kingpin.Flag("results", "maximum number of output results").Short('n').Default("10").Int()
-	redisUrl := kingpin.Arg("url", "redis URL").Default("redis://localhost").String()
+	samples := kingpin.Flag("samples", "Maximum number of samples.").Short('s').Default("1000").Int()
+	results := kingpin.Flag("results", "Maximum number of output results.").Short('n').Default("10").Int()
+	redisUrl := kingpin.Arg("url", "Redis URL.").Default("redis://localhost").String()
 
 	kingpin.Parse()
 
